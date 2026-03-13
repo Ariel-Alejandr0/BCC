65ba8f483cbfd0b0ce38c6c759f9fead0e2a877b
Ariel-Alejandr0 <65261302+Ariel-Alejandr0@users.noreply.github.com>
2026-03-12 21:46:35 -0300

    Update lista_simples.c

--- lista 2 - estrutura de dados/lista_simples.c
+++ lista 2 - estrutura de dados/lista_simples.c
@@ -87,3 +87,61 @@ NoLista *sllLibera(NoLista *head){
     }
     return NULL;
 };
+// versao lista 3
+NoLista *sllInsereFim(NoLista *head, float v){
+    NoLista *no = malloc(sizeof(NoLista));
+    no->info = v;
+    no->prox = NULL;
+
+    if(head == NULL){
+        return no;
+    }
+
+    NoLista *ultimo = sllUltimo(head);
+    ultimo->prox = no;
+
+    return head;
+}
+int sllIgual(NoLista *lista1, NoLista *lista2){
+    if(lista1 == NULL && lista2 == NULL){
+        return 1;
+    }
+    else{
+        if(lista1 == NULL || lista2 == NULL){
+            return 0;
+        }
+        if(lista1->info == lista2->info){
+            return sllIgual(lista1->prox, lista2->prox);
+        }
+        else{
+            return 0;
+        }
+    }
+}
+void sllImprimeRecursivo(NoLista *head){
+    sllImprime(head);
+}
+NoLista *sllRetiraRecursivo(NoLista *head, float v){
+    if(head == NULL){
+        return NULL;
+    }
+
+    if(head->info == v){
+        NoLista *temp = head;
+        head = head->prox;
+        free(temp);
+        return head;
+    }
+
+    head->prox = sllRetiraRecursivo(head->prox, v);
+    return head;
+}
+int sllComprimentoRecursivo(NoLista *head){
+    if(head == NULL){
+        return 0;
+    }
+    return 1 + sllComprimentoRecursivo(head->prox);
+}
+int sllIgualRecursivo(NoLista *lista1, NoLista *lista2){
+    return sllIgual(lista1, lista2);
+}
