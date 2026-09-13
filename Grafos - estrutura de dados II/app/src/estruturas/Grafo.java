2482983976e724185ebdb12feac34c7dd0418995
Ariel-Alejandr0 <pessoal.ariel.alejandro@gmail.com>
2026-09-13 17:30:21 -0300

    Mundaças de arquitetura e finalização dos Métodos de grafos.

--- Grafos - estrutura de dados II/app/src/estruturas/Grafo.java
+++ Grafos - estrutura de dados II/app/src/estruturas/Grafo.java
@@ -1,33 +1,44 @@
 package estruturas;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Grafo {
-    private List<Aresta> arestas;
-    private List<Vertice> vertices;
+    private List<Vertice> vertices = new ArrayList<Vertice>();
+    private List<Aresta> cacheArestas = new ArrayList<Aresta>();
 
     public int getOrdem(){
         return vertices.size();
     }
     public int getTamanho(){
-        return arestas.size();
+        return cacheArestas.size();
     }
-    public void vertices() {
-        vertices.forEach((elemento) -> {
-            System.out.println(elemento);
-        });
+    public List<Vertice> vertices() {
+        return vertices;
     }
 
-    public void arestas() {
-        arestas.forEach((elemento) -> {
-            System.out.println(elemento);
-        });
+    public List<Aresta> arestas() {
+        return cacheArestas;
     }
 
     public Vertice insereV(){
         Vertice novoVertice = new Vertice();
         vertices.add(novoVertice);
         return novoVertice;
+    }
+    public Vertice removeV(Vertice v){
+        if(!vertices.contains(v)){
+            System.out.println("Vertice v não pertence ao grafo.");
+            return null;
+        }
+
+        List<Aresta> arestas = new ArrayList<>(v.getArestas());//copia primeiro
+        for(Aresta a : arestas) {
+            removeA(a);
+        }
+
+        vertices.remove(v);
+        return v;
     }
     public Aresta insereA(Vertice u, Vertice v){
         if(!vertices.contains(u)){
@@ -38,27 +49,41 @@ public class Grafo {
             return null;
         }
         Aresta novaAresta = new Aresta(u, v);
-        arestas.add(novaAresta);
+        u.getArestas().add(novaAresta);
+        v.getArestas().add(novaAresta);
+        cacheArestas.add(novaAresta);
+
         return novaAresta;
     }
-    public void removeA(Aresta e){
-        arestas.remove(e);
+    public boolean removeA(Aresta e){
+        Vertice u = e.getU();
+        Vertice v = e.getV();
+
+        boolean removidaU = u.getArestas().remove(e);
+        boolean removidaV = v.getArestas().remove(e);
+        boolean removidaCache = cacheArestas.remove(e);
+
+        return (removidaU && removidaV && removidaCache);
     }
     public List<Vertice> adj(Vertice v){
-        List<Vertice> adjacentes;
-        arestas.forEach(aresta -> {
-            if(aresta.getU() == v){
-                Vertivce vAdj = aresta.getU();
+        List<Vertice> adjacentes = new ArrayList<Vertice>();
+        if(!vertices.contains(v)){
+            System.out.println("Vertice v não pertence ao grafo.");
+            return null;
+        }
+        for(Aresta a : v.getArestas()){
+            if(a.getU() == v){
+                Vertice vAdj = a.getV();
                 adjacentes.add(vAdj);
-            } else if (aresta.getV() == v){
-                Vertivce vAdj = aresta.getV();
+            } else if (a.getV() == v){
+                Vertice vAdj = a.getU();
                 adjacentes.add(vAdj);
             }
-        });
+        };
         return adjacentes;
     }
     public Aresta getA(Vertice u, Vertice v){
-        for(Aresta A : arestas){
+        for(Aresta A : u.getArestas()){
             if(A.getU() == u && A.getV() == v){
                 return  A;
             } else if(A.getU() == v && A.getV() == u){
@@ -67,6 +92,26 @@ public class Grafo {
         }
         return null;
     }
-    public grauE()
-    
+    public int grauV(Vertice v) {
+        return v.getArestas().size();
+    }
+    public List<Vertice> verticesA(Aresta e){
+        return List.of(e.getU(), e.getV());
+    }
+    public Vertice oposto(Vertice v, Aresta e){
+        if(v == e.getU()){
+            return e.getV();
+        } else if (v == e.getV()){
+            return e.getU();
+        }
+        System.out.println("O vértice v não pertence a aresta.");
+        return null;
+    }
+    public List<Aresta> arestasV(Vertice v){
+        if(!vertices.contains(v)){
+            System.out.println("Vertice v não pertence ao grafo.");
+            return null;
+        }
+        return v.getArestas();
+    }
 }
--- Grafos - estrutura de dados II/app/src/estruturas/Vertice.java
+++ Grafos - estrutura de dados II/app/src/estruturas/Vertice.java
@@ -1,11 +1,16 @@
 package estruturas;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Vertice {
     private List<Aresta> arestas;
 
-    public Vertice(List<Aresta> arestas){
-        this.arestas = arestas;
+    public Vertice() {
+        arestas = new ArrayList<>(  );
+    }
+
+    public List<Aresta> getArestas() {
+        return arestas;
     }
 }
